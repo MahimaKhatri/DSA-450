dd2d68fcaecc544009fc44def8690189d3cece13
Mahima Khatri <mahima8301@gmail.com>
2022-03-01 11:43:57 +0530

    Patterns added

--- Arrays/Q1-Reverse the array.cpp
+++ Arrays/Q1-Reverse the array.cpp
@@ -6,11 +6,11 @@ int main()
 {
     string s ;
     getline(cin,s);
-    
+
     int n = s.length();
-    
+
     int start =0,end=n-1;
-    
+
     while(start< end)
     {
         swap(s[start],s[end]);
